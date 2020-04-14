6d5eb96133ebde8f1841ab888a20d0fd31a2aaf9
zhangbo <xiaobo21@163.com>
2020-04-14 17:30:50 +0800

    init

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,11 @@
 // default config
 export = {
   workers: 1,
-  port: 8001
+  port: 8001,
+  errnoField: 'code', // errno字段
+  errmsgField: 'msg', // errmsg字段
+  defaultErrno: 0, // errcode字段
+  validateDefaultErrno: 1,
+  encoding: 'utf-8',
+  host: '',
 };
