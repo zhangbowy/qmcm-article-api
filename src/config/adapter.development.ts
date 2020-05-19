6d321c0674f27f5d1a0150096c94d5dec96c45a1
zhangbo <xiaobo21@163.com>
2020-05-19 10:02:47 +0800

    database

--- src/config/adapter.development.ts
+++ src/config/adapter.development.ts
@@ -14,15 +14,17 @@ exports.model = {
     },
     mysql: {
         handle: mysql,
-        database: 'yuncixiu_v3', //数据库名
+        database: 'yuncixiu', //数据库名
+        // database: 'yuncixiu_v3', //数据库名
         prefix: '',//表前缀
         encoding: 'utf8',
-        // host: '129.211.65.201',//host
-        host: '192.168.31.4',//host
+        host: '127.0.0.1',//host
+        // host: '192.168.31.4',//host
         port: 3306,//端口
-        // user: 'root',//用户名
-        user: 'cx03',//用户名
-        password: 'yuncixiu03',//密码
+        user: 'yuncixiu',//用户名
+        // user: 'cx03',//用户名
+        password: 'PCSXsX2HCJnfCYEb',//密码
+        // password: 'yuncixiu03',//密码
         dateStrings: true,
         // acquireWaitTimeout: 3000,
         debounce: false,
