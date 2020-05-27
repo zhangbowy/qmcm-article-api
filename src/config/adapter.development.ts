068c29fc87a9484a092027de23477654dc1d4db3
zhangbo <xiaobo21@163.com>
2020-05-27 18:43:13 +0800

    定時任務

--- src/config/adapter.development.ts
+++ src/config/adapter.development.ts
@@ -14,17 +14,17 @@ exports.model = {
     },
     mysql: {
         handle: mysql,
-        database: 'yuncixiu', //数据库名
-        // database: 'yuncixiu_v3', //数据库名
+        // database: 'yuncixiu', //数据库名
+        database: 'yuncixiu_v3', //数据库名
         prefix: '',//表前缀
         encoding: 'utf8',
-        host: '127.0.0.1',//host
-        // host: '192.168.31.4',//host
+        // host: '129.211.65.201',//host
+        host: '192.168.31.4',//host
         port: 3306,//端口
-        user: 'yuncixiu',//用户名
-        // user: 'cx03',//用户名
-        password: 'PCSXsX2HCJnfCYEb',//密码
-        // password: 'yuncixiu03',//密码
+        // user: 'yuncixiu',//用户名
+        user: 'cx03',//用户名
+        // password: 'PCSXsX2HCJnfCYEb',//密码
+        password: 'yuncixiu03',//密码
         dateStrings: true,
         // acquireWaitTimeout: 3000,
         debounce: false,
@@ -40,8 +40,8 @@ exports.cache = {
     },
     redis: {
         handle: redisCache,
-        host: '127.0.0.1',
-        // host: '192.168.31.3',
+        // host: '127.0.0.1',
+        host: '192.168.31.3',
         port: '6379',
         password: '',
         log_connect: true
---
+++ src/config/crontab.ts
@@ -0,0 +1,34 @@
+import {think} from "thinkjs";
+module.exports = [{
+    interval: '10s',
+    immediate: true,
+    enable:true,
+    handle: async () => {
+        console.log('定时任务');
+        let orderList = await think.model('order').where({status:1}).select();
+        let today = new Date().getTime();
+        let now = today;
+        // let oneDay =think.ms('1 days');
+        // let oneDay = 1000*60*60*8 + 1000*60;
+        let oneDay = 1000 * 60 * 30;
+        let list: any = [];
+        for (let order of orderList) {
+            let created_time = new Date(order.created_at).getTime();
+            if (now - created_time > oneDay) {
+                list.push(order);
+                console.log(created_time);
+                await think.model('order').where({id: order.id}).update({
+                    _status:"超时,已取消",
+                    status:-2
+                });
+                await think.model('order_item').where({order_id: order.id}).update({
+                    item_status:-2
+                })
+            }
+        }
+    }
+  }, {
+    cron: '0 */1 * * *',
+    handle: 'crontab/test',
+    type: 'all'
+  }]
