7d3e2251039a443729e7efc5757dbe4964c19ead
zhangbo <xiaobo21@163.com>
2020-06-10 11:10:32 +0800

    扫描机器 下发机器

--- src/controller/wx/base.ts
+++ src/controller/wx/base.ts
@@ -3,7 +3,7 @@ import restController from '../rest';
 export default class extends restController {
   async __before() {
     this.header('Access-Control-Allow-Origin', this.header("origin") || "*");
-    this.header('Access-Control-Allow-Headers', ["x-requested-with", 'origin', 'token', 'content-type']);
+    this.header('Access-Control-Allow-Headers', ["x-requested-with", 'origin', 'token', 'content-type', 'ops']);
     this.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE');
     this.header('Access-Control-Allow-Credentials', true);
     try {
--- src/controller/wx/order.ts
+++ src/controller/wx/order.ts
@@ -1597,13 +1597,13 @@ export default class extends Base {
                 if (think.isEmpty(orderInfo)) {
                     return this.fail(-1, '暂无数据!');
                 }
-                console.log(mechineId, 'machineId')
+                console.log(mechineId, 'machineId');
                 const order_id = orderInfo.id;
                 const order_item = await this.model('order_item').where({ order_id }).find();
                 const res: any = await this.fetch(order_item.design_dst_path);
                 this.ctx.set({
                     // 'Content-Length': isHaveFile.size,
-                    "Content-Disposition": "attachment; filename=" + `${order_item.order_id}.DST`,
+                    "Content-Disposition": "attachment; filename=" + `${orderInfo.order_no}.DST`,
                 });
                 const PassThrough = require('stream').PassThrough;
                 await this.model('order').where({machine_code: mechineId}).update({machine_code: 0});
