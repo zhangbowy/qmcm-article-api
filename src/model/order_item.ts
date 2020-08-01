3e93e0ba1b30041f2d5c535ea2ae08afc3d21600
zhangbo <xiaobo21@163.com>
2020-08-01 19:01:08 +0800

    空格

--- src/model/order_item.ts
+++ src/model/order_item.ts
@@ -21,7 +21,7 @@ export default class extends think.Model {
                 fKey: 'design_id', // machine表
                 key: 'design_id', // 当前表
                 // field: 'custom_category_id,desc,machine_name,machine_code',
-                where: { del: 0},
+                where: {del: 0},
             }
         };
     }
