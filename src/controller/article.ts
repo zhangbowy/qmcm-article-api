e2b4342db7bdfb257c0780fd074fe699ac892fc1
zhangbowy <1751170889@qq.com>
2021-02-18 15:43:57 +0800

    feat: 文章测试

---
+++ src/controller/article.ts
@@ -0,0 +1,53 @@
+import Base from './base';
+import {ancestorWhere} from "tslint";
+// import {think} from "thinkjs";
+const path = require('path');
+const crypto = require('crypto');
+const fs = require('fs');
+const AdmZip = require('adm-zip');
+
+export default class extends Base {
+    async indexAction() {
+        try {
+            // return this.redirect('http://www.wkdao.com', '走错路发现世界,走对路发现自己');
+            // const filepath = path.join(think.ROOT_PATH, 'view/index_index.html');
+            // const res = await this.fetch('http://www.wkdao.com/hangye/504.html#').then($res => $res.text())
+            // return this.ctx.body = res;
+            const article_id = this.get('article_id');
+            const result = await think.model('article').where({article_id}).find();
+            this.assign('current_article', result);
+            return this.display();
+            // return this.success(result, "请求成功!");
+        } catch ($err) {
+            // this.dealErr($err)
+        }
+    }
+
+    /**
+     * 添加文章
+     */
+    addArticleAction() {
+        try {
+            const title = this.post('title');
+            const title = this.post('title');
+            const title = this.post('title');
+            const title = this.post('title');
+            const title = this.post('title');
+            const title = this.post('title');
+        } catch ($err) {
+
+        }
+    }
+
+    /**
+     * 编辑文章
+     * @param
+     */
+    editArticleAction() {
+        try {
+
+        } catch ($err) {
+
+        }
+    }
+}
