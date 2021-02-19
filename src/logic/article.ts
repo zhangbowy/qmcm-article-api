659e945758bbe70297d8dd08d30e2484c7dc917a
zhangbowy <1751170889@qq.com>
2021-02-19 14:58:24 +0800

    feat: 文章发布

--- src/logic/article.ts
+++ src/logic/article.ts
@@ -118,4 +118,25 @@ export default class extends base {
             return this.fail(-1, msg);
         }
     }
+
+    publishAction() {
+        const rules = {
+            article_id: {
+                required: true,
+                method: 'post'
+            },
+            status: {
+                required: true,
+                method: 'post'
+            }
+        };
+        const msgs: object = {
+            required: '{name}不能为空'
+        };
+        if (!this.validate(rules, msgs)) { // 校验不通过
+            const keys: string[] = Object.keys(this.validateErrors);
+            const msg: string = this.validateErrors[keys[0]];
+            return this.fail(-1, msg);
+        }
+    }
 }
