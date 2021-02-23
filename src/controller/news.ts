a38c4b03fd8456eaf4b8a4064fcabcd82e57975d
zhangbo <1751179889@qq.com>
2021-02-23 17:28:31 +0800

    feat: seo_keywords

--- src/controller/news.ts
+++ src/controller/news.ts
@@ -51,7 +51,7 @@ export default class extends think.Controller {
             this.assign('current_cate', current_cate);
             this.assign('category', category);
             this.assign('current_article', result);
-            return this.success({newest_list, hot_list , next_article, prev_article, current_cate, category, current_article: result});
+            // return this.success({newest_list, hot_list , next_article, prev_article, current_cate, category, current_article: result});
             // 增加阅读
             await think.model('article').where({article_no}).increment('pv', 1);
             await think.model('article').where({article_no}).increment('real_pv', 1);
