0a02557a283046017ccb72a92c488b338ea92ff1
zhangbowy <1751170889@qq.com>
2021-02-23 11:19:15 +0800

    feat: 文章数据

--- src/config/router.ts
+++ src/config/router.ts
@@ -1,5 +1,7 @@
 export = [
  // ['/wx/order/orderTrace', '/order/orderTrace']
     ['/wx/aboutUs', '/wx/index/aboutUs'],
-    ['/news/:article_id', 'news']
+    ['/news/:article_id', 'news'],
+    ['/news/cate/:cate_id', '/news/cate']
+
 ];
